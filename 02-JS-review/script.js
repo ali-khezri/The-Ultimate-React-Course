dfc259e08e6e98fcf546cc9570d3a2b2901dc6a4
Ali Khezri <khezri.ali.1994@gmail.com>
2025-12-16 21:39:20 +0330

    Immutable Arrays

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -267,3 +267,23 @@ arr;
 
 const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
 sortedByPages;
+
+// Add a book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the chamber of Secrets",
+  author: "J. K. Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// Delete a book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 116445548 } : book
+);
+booksAfterUpdate;
