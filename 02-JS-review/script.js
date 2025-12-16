31a4661b41696ea38ec279110ec66dac379e8d45
Ali Khezri <khezri.ali.1994@gmail.com>
2025-12-16 22:09:14 +0330

    AsyncAwait

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -290,8 +290,21 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 booksAfterUpdate;
 */
 
-const x = fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// const x = fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log("Ali");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
 
 console.log("Ali");
