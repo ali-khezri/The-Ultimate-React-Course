9ecf490bd9c3d73a42a59a034497d69a987d6d85
Ali Khezri <khezri.ali.1994@gmail.com>
2026-05-04 20:44:20 +0330

    Section Summary Practical Takeaways

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -69,6 +69,21 @@ function TabContent({ item }) {
     setLikes((prevLikes) => prevLikes + 1);
   }
 
+  function handleTripleInc() {
+    setLikes((prevLikes) => prevLikes + 1);
+    setLikes((prevLikes) => prevLikes + 1);
+    setLikes((prevLikes) => prevLikes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -82,13 +97,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
