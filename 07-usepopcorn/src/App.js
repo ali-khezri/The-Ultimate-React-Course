9210649d8b8b88fe4098f4d1655a39d4cef0874e
Ali Khezri <khezri.ali.1994@gmail.com>
2026-06-08 19:44:03 +0330

    Refs to Select DOM Elements

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import { func } from "prop-types";
 
@@ -167,10 +167,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
   useEffect(function () {
-    const el = document.querySelector(".search");
-    el.focus();
-  }, []);
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   el.focus();
+  // }, []);
 
   return (
     <input
@@ -179,6 +195,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
