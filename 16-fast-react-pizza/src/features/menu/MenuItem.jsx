882e0adfe62666e2468aca1784cd04a9af93b7cc
Ali Khezri <khezri.ali.1994@gmail.com>
2026-07-25 19:28:16 +0330

    Fetching Data With React Router Loaders Pizza Menu

--- 16-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,14 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+import { formatCurrency } from "../../utils/helpers";
 
+function MenuItem({ pizza }) {
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  //  id,
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
