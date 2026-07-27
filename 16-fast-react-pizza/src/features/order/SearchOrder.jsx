6b7080a232ff3e00d5cef995a15ab1c60a0fa006
Ali Khezri <khezri.ali.1994@gmail.com>
2026-07-27 15:10:31 +0330

    Styling Form Elements

--- 16-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="rounded-full px-4 py-2 text-sm bg-yellow-100 "
+        className="focus-opacity-50 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all placeholder:text-stone-400 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 "
       />
     </form>
   );
