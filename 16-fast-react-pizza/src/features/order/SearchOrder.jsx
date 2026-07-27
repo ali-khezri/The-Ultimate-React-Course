5bd007ca1f0c26976038867a66626089e3b14670
Ali Khezri <khezri.ali.1994@gmail.com>
2026-07-27 14:39:00 +0330

    Using Flexbox

--- 16-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full px-4 py-2 text-sm bg-yellow-100 "
       />
     </form>
   );
--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Ali</div>;
+  return <div className="hidden text-sm font-semibold md:block">Ali</div>;
 }
 
 export default Username;
