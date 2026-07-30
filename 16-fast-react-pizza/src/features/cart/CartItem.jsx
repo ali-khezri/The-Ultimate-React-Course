770d7c1eafa34085d2a8085ea1a5696873ead0d3
Ali Khezri <khezri.ali.1994@gmail.com>
2026-07-30 20:48:19 +0330

    Deleting Cart Items

--- 16-fast-react-pizza/src/features/cart/CartItem.jsx
+++ 16-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,9 +1,9 @@
-import Button from "../../UI/Button";
 import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
-  const { name, quantity, totalPrice } = item;
-  // pizzaId
+  const { pizzaId, name, quantity, totalPrice } = item;
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
@@ -11,7 +11,8 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
