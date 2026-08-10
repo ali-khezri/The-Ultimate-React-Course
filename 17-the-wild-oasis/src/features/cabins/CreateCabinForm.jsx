e031acea0f2d4cef7fb72ce021f8ae0ea4de3169
Ali Khezri <khezri.ali.1994@gmail.com>
2026-08-10 21:42:19 +0330

    Fixed Bug

--- 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -113,7 +113,6 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow>
-        {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
