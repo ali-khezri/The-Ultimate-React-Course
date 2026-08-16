dd2b11c89e2eda3ebca6127c7166819f427dbf16
Ali Khezri <khezri.ali.1994@gmail.com>
2026-08-16 23:27:39 +0330

    Applying the Render Props Pattern

--- 17-the-wild-oasis/src/ui/Table.jsx
+++ 17-the-wild-oasis/src/ui/Table.jsx
@@ -39,9 +39,9 @@ const StyledRow = styled(CommonRow)`
   }
 `;
 
-// const StyledBody = styled.section`
-//   margin: 0.4rem 0;
-// `;
+const StyledBody = styled.section`
+  margin: 0.4rem 0;
+`;
 
 const Footer = styled.footer`
   background-color: var(--color-grey-50);
@@ -55,12 +55,12 @@ const Footer = styled.footer`
   }
 `;
 
-// const Empty = styled.p`
-//   font-size: 1.6rem;
-//   font-weight: 500;
-//   text-align: center;
-//   margin: 2.4rem;
-// `;
+const Empty = styled.p`
+  font-size: 1.6rem;
+  font-weight: 500;
+  text-align: center;
+  margin: 2.4rem;
+`;
 
 const TableContext = createContext();
 
@@ -91,10 +91,14 @@ function Row({ children }) {
   );
 }
 
-// function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
-// Table.Body = Body;
+Table.Body = Body;
 Table.Row = Row;
 Table.Footer = Footer;
 
